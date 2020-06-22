56f047e96a33818e5cc58c15546fa79ffa24eefe
poojachirp <poojasrinath@gmail.com>
2020-06-21 21:28:37 -0400

    adding room component

---
+++ src/components/Room.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login.js';
+
+function Room() {
+  return (
+    <div className="App">
+     <p>Welcome to the room</p>
+     <div>
+        <div>
+            
+        </div>
+     </div>
+
+    </div>
+  );
+}
+
+export default Room;
