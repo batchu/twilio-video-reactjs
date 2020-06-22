1e4442a06d7cbae7f78ea2cbfcad9e0eb069ac24
Prashanth Batchu <prashanth@batchu.net>
2020-06-22 19:10:58 -0400

    Loading Room after login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-//import Login from './components/Login.js';
-import Room from './components/Room.js';
+import Login from './components/Login.js';
 
 function App() {
   return (
     <div className="App">
-      <Room  name= 'Pooja' />
+     <Login />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Room from "./Room.js"
 function Login() {
 
-    function handleSubmit(event){
-        alert("submitted"+event)
-        event.preventDefault();
-    }
-  return (
-    <div>
+  const [login, setLogin] = useState({ login: false });
+  const [name, setName] = useState({ name: "" });
+
+  function handleSubmit(event) {
+    setLogin(true)
+    setName(event.target[0].value)
+    event.preventDefault();
+  }
+
+  if (login === true) {
+    return (<Room name={name} />)
+  }
+
+  else
+    return (<div>
       <p>Please Login</p>
-      <form  onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" name="name" />
+        <input type="text" name="name" />
         </label>
         <input type="submit" value="Join my room" />
       </form>
     </div>
-  );
+    )
+
 }
 
 export default Login;
