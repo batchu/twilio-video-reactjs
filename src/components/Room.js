04705b6ccdd81cf3714a9204e06aefbe51b256f7
poojachirp <poojasrinath@gmail.com>
2020-06-22 19:57:59 -0400

    adding twilio video apis

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Room.css";
 
-
 function Room(props) {
-  const participants = []; 
+  const participants = [];
+  
+  const { connect } = require('twilio-video');
+
+connect('$TOKEN', { name:'my-new-room' }).then(room => {
+  console.log(`Successfully joined a Room: ${room}`);
+  room.on('participantConnected', participant => {
+    console.log(`A remote Participant connected: ${participant}`);
+  });
+}, error => {
+  console.error(`Unable to connect to Room: ${error.message}`);
+});
+  
   participants.push(props.name);
   const participantsItems = participants.map((name) => 
     <li>{name}</li>
